8fe81eb1e5bfbb7bf93c59c394d08ece1a3cbe50
Ganesh Prasad R a.k.a TheBluePhilosoph3r <ganesh21111999@gmail.com>
2021-09-19 21:11:57 +0530

    awesome

--- 2_randomMoverWith@obj.js
+++ 2_randomMoverWith@obj.js
@@ -1,17 +1,20 @@
 xPos = 100;
 yPos = 100;
-xSpeed = 1.0;
+xSpeed = 1;
 ySpeed = 2.1;
 canvasXSize = 800;
 canvasYSize = 400;
 sizeOfObj = 21;
-
+backgroundGreyscale = 0;
+CanvasBgColorRed = 220;
+CanvasBgColorGreen = 0;
+CanvasBgColorBlue = 120;
 function setup() {
   createCanvas(canvasXSize, canvasYSize);
 }
 
 function draw() {
-  background(0);
+  background(CanvasBgColorRed,CanvasBgColorGreen);
 
   xPos = xPos + xSpeed;
   yPos = yPos + ySpeed;
@@ -22,5 +25,8 @@ function draw() {
   if (yPos >= canvasYSize || yPos <= 0) {
     ySpeed *= -1;
   }
+  //fill(random(122),random(10),random(100),random(0,23))
+  //stroke(0)
   circle(xPos, yPos, sizeOfObj);
+  square(canvasXSize - xPos,canvasYSize - yPos, sizeOfObj);
 }
